7fb561ada49aca7894ee5a3283207ec0cc0b4e81
Tudose <vasile.tudose@student.unitbv.ro>
2025-07-16 19:32:30 +0300

    refactor Coach and FitnessClass classes to use Lombok annotations; add FitnessClassCoachDetailsDto for enhanced DTO mapping

--- src/main/java/com/example/spring_boot_tutorial/Aexposition/dto/CoachDto.java
+++ src/main/java/com/example/spring_boot_tutorial/Aexposition/dto/CoachDto.java
@@ -1,15 +1,17 @@
 package com.example.spring_boot_tutorial.Aexposition.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CoachDto {
     String id;
     String name;
+    // return also the details of fitness class of coach
+    public List<FitnessClassCoachDetailsDto> fitnessClasses;
 }
---
+++ src/main/java/com/example/spring_boot_tutorial/Aexposition/dto/FitnessClassCoachDetailsDto.java
@@ -0,0 +1,24 @@
+package com.example.spring_boot_tutorial.Aexposition.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class FitnessClassCoachDetailsDto {
+
+    String name;
+    String id;
+    String duration;
+
+    // added method to format the display the hour format nicely when return data in dto
+    public String appendHourToDuration(String duration) {
+        if (duration.equals("1")) {
+            return duration + " hour";
+        }
+        return duration + " hours";
+    }
+
+}
